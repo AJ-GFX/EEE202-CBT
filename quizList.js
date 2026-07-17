8990cf43ac5416916c3ddaa3e03f5758117e5c60
AJ-GFX <ajayiajibolaisreal@gmail.com>
2026-07-17 03:58:25 +0100

    Add files via upload

---
+++ quizList.js
@@ -0,0 +1,27 @@
+const quizBanks = [
+
+    {
+        name: "Magnetism",
+        file: "Data/magnetism.json",
+        description: "Magnetic fields, forces, flux and calculations"
+    },
+
+    {
+        name: "Magnetism Calculations",
+        file: "Data/magnetismCal.json",
+        description: "Magnetic fields, forces, flux and calculations"
+    },
+
+    {
+        name: "Circuits",
+        file: "Data/circuits.json",
+        description: "AC/DC circuits, resistors, capacitors and inductors"
+    },
+
+    {
+        name: "Transformers",
+        file: "Data/transformer.json",
+        description: "Transformer theory, equations and applications"
+    }
+
+];
